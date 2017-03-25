74cd65e556468682db79afdd32ed2b9cf3c82b6d
WonChung <wchung3@swarthmore.edu>
2017-03-25 11:08:45 -0400

    location

--- HacktheTrico/Map1ViewController.swift
+++ HacktheTrico/Map1ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class Map1ViewController: UIViewController {
+class Map1ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        // set initial location to current location
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }
+        
     }
 
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.map.setRegion(region, animated: true)
+    }
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
